404b1a3c9fd773028b92032e90c9d04dc41ce56e
Cameron Pickle <cpickle@dealersocket.com>
2018-05-14 08:54:07 -0600

    gradients in canvas

--- gradients/script.js
+++ gradients/script.js
@@ -2,11 +2,14 @@ function initiate() {
     let elem = document.getElementById('canvas');
     let canvas = elem.getContext('2d');
 
-    canvas.fillStyle = "#0000ff";
-    canvas.strokeStyle = "#ff0000";
-    canvas.strokeRect(100, 100, 120, 120);
-    canvas.fillRect(110, 110, 100, 100);
-    canvas.clearRect(130, 130, 30, 30);
+    let gradient = canvas.createLinearGradient(0, 0, 10, 100);
+    gradient.addColorStop(0.5, '#0088ff');
+    gradient.addColorStop(1, '#ff0000');
+    canvas.fillStyle = gradient;
+
+    canvas.fillRect(150, 10, 200, 100);
+    canvas.fillRect(400, 10, 200, 100);
+    canvas.fillRect(400, 150, 100, 100);
 }
 
 addEventListener("load", initiate);
